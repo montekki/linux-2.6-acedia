montekki/linux-2.6-acedia

commit 47c593ee5d95ea8f0cb709d952061afff791236d

avoid the unnecessary utrace_resume()->utrace_reset()
utrace_resume(UTRACE_REPORT) always calls utrace_reset() because
start_callback() obviously can't clear report->spurious when
event == 0.

Change start_callback() to correctly clear ->spurious in this case.
We could probably clear it in utrace_resume() unconditionally after
reporting loop, but this is not exactly right if there are no engines
which have QUIESCE in engine->flags.

utrace_get_signal() probably has the same problem.

Note: utrace_control(DETACH) does utrace_do_stop() and sets UTRACE_REPORT
if the tracee is not stopped. It also does mark_engine_detached() which
does not set QUIESCE in target->utrace_flags. This means we rely on
report.spurious which should provoke utrace_reset() from utrace_resume()
if target->utrace_flags doesn't have QUIESCE. A bit too subtle, imho.
Also, UTRACE_REPORT can be lost because of UTRACE_INTERRUPT or normal
signal: utrace_get_signal() checks "utrace_flags & UTRACE_EVENT(QUIESCE)"
and returns otherwise. This should be fixed somehow. This check is wrong
anyway, but it is not clear how we can fix the race with DETACH.

Signed-off-by: Oleg Nesterov <oleg@redhat.com>
Signed-off-by: Roland McGrath <roland@redhat.com>

kernel/utrace.c
@@ -1527,7 +1527,7 @@ static const struct utrace_engine_ops *start_callback(
 	if (want & ENGINE_STOP)
 		report->action = UTRACE_STOP;
 
-	if (want & event) {
+	if (want & (event ?: UTRACE_EVENT(QUIESCE))) {
 		report->spurious = false;
 		return ops;
 	}

commit 30c9e5584d662993b6ecad97956d0d108f5ed1fe

utrace: utrace_resume()->start_callback() must clear ->reporting
utrace_resume()->start_callback() can return without clearing
->reporting, this is very wrong. The bug was introduced by me in
47c593ee "avoid the unnecessary utrace_resume()->utrace_reset()"
commit.

Revert this patch and change start_callback() to check event right
after we call ->report_quiesce(). If it is zero we can just clear
->spurious and return without playing with ->reporting and ->flags.
No need to worry about ENGINE_STOP, finish_callback() has already
updated engine->flags and report->action correctly.

Signed-off-by: Oleg Nesterov <oleg@redhat.com>
Signed-off-by: Roland McGrath <roland@redhat.com>

kernel/utrace.c
@@ -1512,6 +1512,12 @@ static const struct utrace_engine_ops *start_callback(
 							   engine, event)))
 			return NULL;
 
+		if (!event) {
+			/* We only got here to report QUIESCE */
+			report->spurious = false;
+			return NULL;
+		}
+
 		/*
 		 * finish_callback() reset utrace->reporting after the
 		 * quiesce callback.  Now we set it again (as above)
@@ -1527,7 +1533,7 @@ static const struct utrace_engine_ops *start_callback(
 	if (want & ENGINE_STOP)
 		report->action = UTRACE_STOP;
 
-	if (want & (event ?: UTRACE_EVENT(QUIESCE))) {
+	if (want & event) {
 		report->spurious = false;
 		return ops;
 	}